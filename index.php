3fe87a6304c77fd970a82dcf93ebe04888b35e43
DavideCotigliani <davide.cotigliani@gmail.com>
2025-07-16 17:47:37 +0200

    [UPD] Milestone 1 completed

--- index.php
+++ index.php
@@ -15,14 +15,36 @@
                 <button type="submit">Invia</button>
             </div> 
     </form>
+    <?php
+        $password = "";
+    if ($password != "") {
+        ?>
+    <h2>La tua password:</h2>
+    <pre> <?php echo $password ?> </pre>
+    <?php
+    }
+    ?>
 
     <?php
     if (isset($_GET['password'])) {
-        //funzione
-        function myPassword()
-        {
 
+        $minuscole = 'abcdefghijklmnopqrstuvwxyz';
+        $maiuscole = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $numeri = '1234567890';
+        $simboli = '!?~@#-_+<>[]{}';
+
+        //racchiudo tutti i caratteri necessari in una variabile
+        $tuttiCaratteri = $minuscole . $maiuscole . $numeri . $simboli;
+
+
+        for ($i = 0; $i < $_GET['password'];$i++) {
+            //Prende carattere random da questa stringa
+            $randomPosition = rand(1, strlen($tuttiCaratteri) - 1);
+            $randomCharacter = substr($tuttiCaratteri, $randomPosition, 1);
+            //aggiunge questo carattere alla stringa della password per tot volte
+            $password .= $randomCharacter;
         }
+        echo $password;
     }
     ?>
 </body>
